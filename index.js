461947db80480042fdaf81978a12c16b93ba176a
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-04-19 12:04:36 +0600

    done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -27,8 +29,30 @@ async function run() {
 
     app.post("/applicationData", async (req, res) => {
       const data = req.body;
-      console.log(data);
       const result = await applicationData.insertOne(data);
+      const { email } = data;
+
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "ferne46@ethereal.email",
+          pass: "JvmHad27Yn7VRGaes2",
+        },
+      });
+      const mailOptions = {
+        from: "tanjirtuhin18@gmail.com",
+        to: { email },
+        subject: "Application Feedback",
+        text: `Name: ${data.name}\nEmail: ${data.email}\nMessage: ${data.message}`,
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error("Error sending email:", error);
+        } else {
+          console.log("Email sent:", info.response);
+        }
+      });
+
       res.send(result);
     });
   } catch (err) {
